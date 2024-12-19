d66b1635202a64bfba77f2a2b23d17d66a5bd845
Shadyar Bakr <Shadyar.bakr@proton.me>
2024-12-19 19:14:04 +0300

    Implement error handling and JSON responses in greenlight API
    - Added error handling functions for logging and responding to errors in the API.
    - Introduced a new `errors.go` file for centralized error management.
    - Updated the healthcheck handler to return structured JSON responses.
    - Implemented JSON reading and writing utilities in `helpers.go`.
    - Created a new `middleware.go` file for panic recovery middleware.
    - Enhanced the movie creation and retrieval handlers to handle JSON input and output.
    - Added a `Movie` struct and a custom `Runtime` type in the internal data package.
    - Updated `go.mod` to remove unnecessary comments and changed the default task in `Taskfile.yml` to use `air` for live reloading.
    
    Signed-off-by: Shadyar Bakr <Shadyar.bakr@proton.me>

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,18 +3,46 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/shadyar-bakr/greenlight/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
